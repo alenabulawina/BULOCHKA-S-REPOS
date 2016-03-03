b583bbc5a1bfaf6d64ece0a9b196b7bcd2548da5
alenabulawina <lelik.bulka@yandex.ru>
2016-03-03 13:30:57 +0300

    umnozhali,plusovali,minusovali

--- main.c
+++ main.c
@@ -6,6 +6,8 @@ int main()
 {
     printf("Vvedite chisla ");
     scanf ("%d,%d",&x, &y);
-    printf ("%d + %d = %d", x,y,x+y);
+    printf ("%d + %d = %d\n", x,y,x+y);
+     printf ("%d - %d = %d\n", x,y,x-y);
+      printf ("%d * %d = %d\n", x,y,x*y);
     return 0;
 }
