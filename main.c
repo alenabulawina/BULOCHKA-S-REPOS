1b755377182915dea867c1d897dbdef3c28dddf5
alenabulawina <lelik.bulka@yandex.ru>
2016-03-09 13:29:24 +0300

    PEREDELALA NORMALNO

--- main.c
+++ main.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 float x,y;
+
 //eto uchebnaya programma//
 int main()
 {
-    printf("Vvedite chisla ");
-    scanf ("%f,%f",&x, &y);
-    printf ("%f + %f = %f\n", x,y,x+y);
-     printf ("%f - %f = %f\n", x,y,x-y);
-      printf ("%f * %f = %f\n", x,y,x*y);
-    printf ("%f / %f = %3.2f\n", x,y,x/y);
-    return 0;
+  printf("Vvedite chisla ");
+  scanf ("%f,%f",&x, &y);
+  printf ("%f + %f = %f\n", x,y,x+y);
+  printf ("%f - %f = %f\n", x,y,x-y);
+  printf ("%f * %f = %f\n", x,y,x*y);
+  printf ("%f / %f = %3.2f\n", x,y,x/y);
+
+  return 0;
 }
